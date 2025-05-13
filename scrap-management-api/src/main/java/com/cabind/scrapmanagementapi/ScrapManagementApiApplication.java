3e8a87e450dec14d2fa6c41291c5f0610de6a67f
AnasHannani <anashannani02@gmail.com>
2025-05-13 09:55:06 +0100

    Initial commit: setup Maven project structure and create packages structure (config, controller, service, repository,model, dto, security, exception, util, audit)

---
+++ scrap-management-api/src/main/java/com/cabind/scrapmanagementapi/ScrapManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.cabind.scrapmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapManagementApiApplication.class, args);
+	}
+
+}
---
+++ scrap-management-api/src/test/java/com/cabind/scrapmanagementapi/ScrapManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabind.scrapmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
